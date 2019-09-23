8b02a22b2da072b185eb0b2efc42ada7d280ffa5
satya surya sai pavan korubilli <saipavan5th@gmail.com>
2019-09-23 20:50:03 +0530

    Update program 14.cpp

--- program 14.cpp
+++ program 14.cpp
@@ -1,3 +1,6 @@
+/*14)Read a line and print the number of words present in the line string.
+   Note that one or more spaces could be present between the wokds */
+
 #include<stdio.h>
 #include<string.h>
 int main() {
@@ -5,7 +8,7 @@ int main() {
 	int count = 0,i;
 	gets(s);
 	for (i = 0;i < strlen(s);i++) {
-		 if (s[i] == "\0" ) {
+		 if (s[i] == "\0" && s[i + 1] != "\0") {
 		 	    count++;
 		 }
 		 
