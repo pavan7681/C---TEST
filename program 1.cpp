9fe283b591e9ff5e1f20910fe87328bb0e0607c2
satya surya sai pavan korubilli <saipavan5th@gmail.com>
2019-09-23 20:34:55 +0530

    Update program 1.cpp

--- program 1.cpp
+++ program 1.cpp
@@ -1,10 +1,19 @@
+/*1)Read two integers from the user (that is first define two integer variables
+   and then read). Perform all possible conditional operations on these
+   two integers. Print the results of all these conditional operations.
+   Following are the possible conditinal operators:
+
+   Equal to(==)    Less than(<)    Greater than(>)   
+   Less than or Equal to (<=)  Greater than or equal to(>=)
+   Not equal to(!=) */
+
 #include<stdio.h>
 int main() {
 	int a,b;
 	printf("enter a and b values :");
 	scanf("%d %d",&a,&b);
-	if (a >= b) {
-		printf("\na is greater than or equal to b");
+	if (a > b) {
+		printf("\na is greater than b");
 	}
 	else {
 		printf("\nb is greater than a");
@@ -12,7 +21,10 @@ int main() {
 	if (a != b) {
 		printf("\na is not equal to b");
 	}
-	//some operations
+        if( a == b ) {
+                printf("\na and b are equal");
+	}
+	//relational operators in nested if.
 	if (a >= 5) {
 		if (a != 2) {
 			a == 0;
