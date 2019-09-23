44c3a0c0b53d9a514d7abe69a7a15bfcf02b90e0
satya surya sai pavan korubilli <saipavan5th@gmail.com>
2019-09-23 20:40:57 +0530

    Update program 5.cpp

--- program 5.cpp
+++ program 5.cpp
@@ -1,3 +1,7 @@
+/*5) Ask user to enter a capital letter. Read the capital letter into a char
+    variable. Set the 5th bit in this char variable to 1. Now print this char
+    as character.*/
+
 #include<stdio.h>
 int main() {
    char ch;
